6b1296feb90f3a294fb2bd07b963326bca9d9a7e
Bohdan <te.dark@mail.ru>
2014-09-21 14:40:17 +0300

    Moved creating ducks to Factory

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -11,38 +11,33 @@ namespace DependencyInjection
     {
         static void Main(string[] args)
         {
-            Duck littleDuck = new RubberDuck();
-            Duck woodenDuck = new WoodenDuck();
-            Duck simleDuck = new SimpleDuck();
-            Duck fakeDuck = new FakeDuck();
+            var ducks = new DuckFactory().CreateDucks();
 
-            Console.WriteLine("1st duck");
-            littleDuck.PerformFly();
-            littleDuck.PerformQuack();
-            littleDuck.Swim();
-
-            Console.WriteLine("\n");
-
-            Console.WriteLine("2nd duck");
-            woodenDuck.PerformFly();
-            woodenDuck.PerformQuack();
-            woodenDuck.Swim();
-
-            Console.WriteLine("\n");
-
-            Console.WriteLine("3rd duck");
-            simleDuck.PerformFly();
-            simleDuck.PerformQuack();
-            simleDuck.Swim();
-
-            Console.WriteLine("\n");
-
-            Console.WriteLine("4th duck");
-            fakeDuck.PerformFly();
-            fakeDuck.PerformQuack();
-            fakeDuck.Swim();
+            for (int i = 0; i < ducks.Count; i++)
+            {
+                DoDuckThings(ducks[i], i+1);
+            }
 
             Console.ReadKey();
+        }
+
+        private static void DoDuckThings(Duck duck, int number)
+        {
+            Console.WriteLine(number + " duck");
+
+            duck.PerformFly();
+            duck.PerformQuack();
+            duck.Swim();
+
+            Console.WriteLine("\n");
+        }
+    }
+
+    internal class DuckFactory
+    {
+        public List<Duck> CreateDucks()
+        {
+            return new List<Duck> {new RubberDuck(), new WoodenDuck(), new SimpleDuck(), new FakeDuck()};
         }
     }
 }

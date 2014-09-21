6b08c8b7cefddb8c7c68de97548498c7cba2dfb7
Bohdan <te.dark@mail.ru>
2014-09-21 14:32:28 +0300

    Solution builds now :)

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FirstTry.strategyPattern;
 
 namespace DependencyInjection
 {
@@ -10,6 +11,38 @@ namespace DependencyInjection
     {
         static void Main(string[] args)
         {
+            Duck littleDuck = new RubberDuck();
+            Duck woodenDuck = new WoodenDuck();
+            Duck simleDuck = new SimpleDuck();
+            Duck fakeDuck = new FakeDuck();
+
+            Console.WriteLine("1st duck");
+            littleDuck.PerformFly();
+            littleDuck.PerformQuack();
+            littleDuck.Swim();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("2nd duck");
+            woodenDuck.PerformFly();
+            woodenDuck.PerformQuack();
+            woodenDuck.Swim();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("3rd duck");
+            simleDuck.PerformFly();
+            simleDuck.PerformQuack();
+            simleDuck.Swim();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("4th duck");
+            fakeDuck.PerformFly();
+            fakeDuck.PerformQuack();
+            fakeDuck.Swim();
+
+            Console.ReadKey();
         }
     }
 }

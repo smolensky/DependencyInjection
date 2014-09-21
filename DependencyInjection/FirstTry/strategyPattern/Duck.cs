227a67589997fb2ac03c64290b717be4e5a8fd79
Bohdan <te.dark@mail.ru>
2014-09-21 14:29:26 +0300

    Copied strategy folder from patterns solution

---
+++ DependencyInjection/FirstTry/strategyPattern/Duck.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstTry.strategyPattern
+{
+    public abstract class Duck
+    {
+        public abstract void Swim();
+
+        public IFlyBehavior FlyBehavior;
+        public IQuackBehavior QuackBehavior;
+
+        public void PerformFly()
+        {
+            FlyBehavior.Fly();
+        }
+
+        public void PerformQuack()
+        {
+            QuackBehavior.Quack();
+        }
+    }
+}
